fcc1e9ec264ee4127be5dbf69f322d7e3bfd7da0
austenhasty <hastyae@gmail.com>
2020-10-29 15:24:37 -0700

    Created FractionCalculator class and began filling out main method. Need to finish creating final three methods

---
+++ src/FractionCalculator.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class FractionCalculator {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("This program is a fraction calculator");
+        System.out.println("It will add, subtract, multiply and divide fractions until you type Q to quit.");
+        System.out.println("Please enter your fractions in the form a/b, where a and b are integers.");
+        System.out.println("--------------------------------------------------------------------------------");
+    }
+
+    public static String getOperation(Scanner input) {
+
+    }
+
+    public static boolean validFraction(String input) {
+
+    }
+
+    public static Fraction getFraction(Scanner input) {
+
+    }
+}
